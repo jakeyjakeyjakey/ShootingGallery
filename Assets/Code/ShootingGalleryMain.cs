11aece20bed2bf759711f9c09fd0ddab7ca1b4a5
jakeyjakeyjakey <112137688+jakeyjakeyjakey@users.noreply.github.com>
2023-01-31 11:11:52 -0500

    Completed

--- Assets/Code/ShootingGalleryMain.cs
+++ Assets/Code/ShootingGalleryMain.cs
@@ -12,27 +12,39 @@ public class ShootingGalleryMain : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        for (int i = 0; i < targetCount; i++)
+        //for (int i = 0; i < targetCount; i++)
         {
-           Transform newTarget = Instantiate(targetPrefab);
-            targets.Add(newTarget);
+            //Transform newTarget = Instantiate(targetPrefab);
+            //targets.Add(newTarget);
         }
-
-        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit rch;
-
-        if (Physics.Raycast(ray, out rch))
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Bonk!");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit rch;
+            GameObject g = null;
+
+            if (Physics.Raycast(ray, out rch))
+            {
+                Debug.DrawLine(ray.origin, rch.point);
+                Debug.Log("Bonk!");
+
+                targets.Remove(rch.transform);
+                Destroy(rch.transform.gameObject);
+            }
+            else
+            {
+                Debug.DrawLine(ray.origin, rch.point, Color.red);
+            }
         }
 
-        rch.transform
+
+
     }
 }
